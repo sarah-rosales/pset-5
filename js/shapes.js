eb5344aa745d5426a4b58d561628b255280113b5
Sarah Rosales <srosales@ucvts.org>
2019-12-03 12:21:56 -0500

    initial commit

--- js/shapes.js
+++ js/shapes.js
@@ -18,8 +18,10 @@ window.onload = function() {
     // there are six event listeners being added for the staff solutions. you'll have an
     // equivalent set of six event listeners for your solutions. the first one is done for you.
 
+//delete this comment later but *scream* tis initial commit
+
     document.getElementById("hello").onclick = sayHello;
-} 
+}
 
 /*
  * Exercise 1.
