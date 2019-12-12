741b86d1666dd6e0abcc2385ca048913ae76f00e
Sarah Rosales <srosales@ucvts.org>
2019-12-11 19:38:17 -0500

    exercise 2 is partially working

--- js/shapes.js
+++ js/shapes.js
@@ -24,14 +24,13 @@ window.onload = function() {
     document.getElementById("triangle").onclick = drawTriangle;
     document.getElementById("smile").onclick = drawFaceStaff;
     document.getElementById("pyramid").onclick = drawPyramid;
-
 }
 
 /*
  * Exercise 1.
  */
 
-const sayHello = function() {
+const sayHello = function draw() {
 
   let sign = prompt("Message:");
   let len = null;
@@ -65,48 +64,38 @@ const drawRectangle = function() {
   let x_coordinate = prompt("X:");
   let y_coordinate = prompt("Y:");
 
-  while (rect_width <= 1 && rect_width >= width) {
-    alert("Your width must be between 1 and 1024.");
-    rect_width = prompt("Width:");
+  if (x_coordinate == null || y_coordinate == null || rect_width == null || rect_height == null) {
+  } else {
+    if (rect_width < 1 && rect_width > width) {
+      alert("Your width must be between 1 and 1024.");
+      rect_width = prompt("Width:");
+    } else if (Number.isNaN(rect_width) && !Number.isInteger(rect_width)) {
+      alert("One of your values is not a number.");
+      rect_width = prompt("Width:");
+    } else if (rect_height < 1 && rect_height > height) {
+      alert("Your height must be between 1 and 1024.");
+      rect_height = prompt("Height:");
+    } else if (Number.isNaN(rect_height) && !Number.isInteger(rect_height)) {
+      alert("One of your values is not a number.");
+      rect_height = prompt("Height:");
+    } else if (x_coordinate < 1 && x_coordinate > width) {
+      alert("Your x-coordinate must be between 1 and 1024.");
+      x_coordinate = prompt("X:");
+    } else if (Number.isNaN(x_coordinate) && !Number.isInteger(x_coordinate)) {
+      alert("One of your values is not a number.");
+      x_coordinate = prompt("X:");
+    } else if (y_coordinate < 1 && y_coordinate > height) {
+      alert("Your y-coordinate must be between 1 and 1024.");
+      y_coordinate = prompt("Y:");
+    } else if (Number.isNaN(y_coordinate) && !Number.isInteger(y_coordinate)) {
+      alert("One of your values is not a number.");
+      y_coordinate = prompt("Y:");
+    }
   }
-
-  while (Number.isNaN(rect_width) && !Number.isInteger(rect_width)) {
-    alert("One of your values is not a number.");
-    rect_width = prompt("Width:");
-  }
-
-  while (rect_height <= 1 && rect_height >= height) {
-    alert("Your height must be between 1 and 1024.");
-    rect_height = prompt("Height:");
-  }
-
-  while (Number.isNaN(rect_height) && !Number.isInteger(rect_height)) {
-    alert("One of your values is not a number.");
-    rect_height = prompt("height:");
-  }
-
-  while (x_coordinate <= 1 && x_coordinate >= width) {
-    alert("Your x-coordinate must be between 1 and 1024.");
-    x_coordinate = prompt("X:");
-  }
-
-  while (Number.isNaN(x_coordinate) && !Number.isInteger(x_coordinate)) {
-    alert("One of your values is not a number.");
-    x_coordinate = prompt("X:");
-  }
-  while (y_coordinate <= 1 && y_coordinate >= height) {
-    alert("Your y-coordinate must be between 1 and 1024.");
-    y_coordinate = prompt("Y:");
-  }
-
-  while (Number.isNaN(y_coordinate) && !Number.isInteger(y_coordinate)) {
-    alert("One of your values is not a number.");
-    y_coordinate = prompt("Y:");
-  }
-
   const ctx = document.getElementById("student-canvas-2").getContext("2d");
-    ctx.clearRect(0, 0, 1024, 512);
-    ctx.strokeRect(x_coordinate, y_coordinate, rect_width, rect_height);
+      ctx.clearRect(0, 0, 1024, 512);
+      ctx.strokeRect(x_coordinate, y_coordinate, rect_width, rect_height);
+
 };
 
 /*
@@ -141,7 +130,7 @@ const drawTriangle = function() {
  */
 
 const drawFace = function() {
-    // write your exercise 5 code here
+    // write your exercise 4 code here
 };
 
 /*
@@ -149,5 +138,5 @@ const drawFace = function() {
  */
 
 const drawPyramid = function() {
-    // write your exercise 6 code here
+    // write your exercise 5 code here
 };
